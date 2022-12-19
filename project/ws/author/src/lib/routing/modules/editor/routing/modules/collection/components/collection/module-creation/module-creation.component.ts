ca6d87522eb02e1b04bbd196dd345e26025b4d39
dakshayini-krishnappa <dakshayini.krishnappa@tarento.com>
2022-12-19 13:05:40 +0530

    updated code push

--- project/ws/author/src/lib/routing/modules/editor/routing/modules/collection/components/collection/module-creation/module-creation.component.ts
+++ project/ws/author/src/lib/routing/modules/editor/routing/modules/collection/components/collection/module-creation/module-creation.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms'
 import { MatDialog } from '@angular/material/dialog'
+import { ConfigurationsService, ImageCropComponent } from '@ws-widget/utils'
+import { IMAGE_MAX_SIZE, IMAGE_SUPPORT_TYPES } from '@ws/author/src/lib/constants/upload'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { NotificationComponent } from '@ws/author/src/lib/modules/shared/components/notification/notification.component'
+import { Notify } from '@ws/author/src/lib/constants/notificationMessage'
+import { NOTIFICATION_TIME } from '@ws/author/src/lib/constants/constant'
+import { LoaderService } from '../../../../../../../../../services/loader.service'
 
 @Component({
   selector: 'ws-author-module-creation',
@@ -51,6 +58,7 @@ export class ModuleCreationComponent implements OnInit {
   isLinkPageEnabled: boolean = false
   isOnClickOfResourceTypeEnabled: boolean = false;
   resourceForm: FormGroup
+  moduleForm!: FormGroup
   resourceImg: string = '';
   isLinkFieldEnabled: boolean = false;
   moduleName: string = '';
@@ -58,11 +66,18 @@ export class ModuleCreationComponent implements OnInit {
   resourceCount: number = 0;
   independentResourceNames: any = [];
   independentResourceCount: number = 0;
+  imageTypes = IMAGE_SUPPORT_TYPES
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog,
+    private configSvc: ConfigurationsService,
+    private snackBar: MatSnackBar,
+    private loader: LoaderService,) {
     this.resourceForm = new FormGroup({
       resourceName: new FormControl(''),
       resourceLinks: new FormControl(''),
+      appIcon: new FormControl('')
+    })
+    this.moduleForm = new FormGroup({
       appIcon: new FormControl('')
     })
   }
@@ -117,6 +132,71 @@ export class ModuleCreationComponent implements OnInit {
     this.showAddModuleForm = true
     this.independentResourceCount = this.independentResourceCount + 1
     this.independentResourceNames.push({ name: 'Resource ' + this.independentResourceCount })
+  }
+
+  changeToDefaultImg($event: any) {
+    $event.target.src = this.configSvc.instanceConfig
+      ? this.configSvc.instanceConfig.logos.defaultContent
+      : ''
+  }
+
+  uploadAppIcon(file: File) {
+    const formdata = new FormData()
+    const fileName = file.name.replace(/[^A-Za-z0-9.]/g, '')
+    if (
+      !(
+        IMAGE_SUPPORT_TYPES.indexOf(
+          `.${fileName
+            .toLowerCase()
+            .split('.')
+            .pop()}`,
+        ) > -1
+      )
+    ) {
+      this.snackBar.openFromComponent(NotificationComponent, {
+        data: {
+          type: Notify.INVALID_FORMAT,
+        },
+        duration: NOTIFICATION_TIME * 1000,
+      })
+      return
+    }
+
+    if (file.size > IMAGE_MAX_SIZE) {
+      this.snackBar.openFromComponent(NotificationComponent, {
+        data: {
+          type: Notify.SIZE_ERROR,
+        },
+        duration: NOTIFICATION_TIME * 1000,
+      })
+      return
+    }
+
+    const dialogRef = this.dialog.open(ImageCropComponent, {
+      width: '70%',
+      data: {
+        isRoundCrop: false,
+        imageFile: file,
+        width: 265,
+        height: 150,
+        isThumbnail: true,
+        imageFileName: fileName,
+      },
+    })
+    dialogRef.afterClosed().subscribe({
+      next: (result: File) => {
+        if (result) {
+          formdata.append('content', result, fileName)
+          this.loader.changeLoad.next(true)
+
+          let randomNumber = ''
+          // tslint:disable-next-line: no-increment-decrement
+          for (let i = 0; i < 16; i++) {
+            randomNumber += Math.floor(Math.random() * 10)
+          }
+        }
+      }
+    })
   }
 
 }
