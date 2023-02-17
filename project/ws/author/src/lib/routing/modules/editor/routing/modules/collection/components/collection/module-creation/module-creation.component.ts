cc41dbdd63930a4df121cead5a1def52e2c4c6a2
likhithgowda <likhith.thammegowda@tarento.com>
2023-02-17 15:01:39 +0530

    bug fix

--- project/ws/author/src/lib/routing/modules/editor/routing/modules/collection/components/collection/module-creation/module-creation.component.ts
+++ project/ws/author/src/lib/routing/modules/editor/routing/modules/collection/components/collection/module-creation/module-creation.component.ts
@@ -2189,11 +2189,10 @@ export class ModuleCreationComponent implements OnInit, AfterViewInit {
 
       var res = this.editResourceLinks.match(/^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/)
       // var res = this.editResourceLinks.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)
-      console.log("res", res)
       if (res !== null) {
         meta["artifactUrl"] = this.editResourceLinks
       }
-      if (res == null) {
+      if (res == null && this.content.mimeType === 'text/x-url') {
         this.snackBar.openFromComponent(NotificationComponent, {
           data: {
             type: Notify.LINK_IS_INVALID,
