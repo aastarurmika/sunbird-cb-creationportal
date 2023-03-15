ff6a96d4722bb578ebef3c7d31f750ade6dbb83e
sprakashg22 <89121816+sprakashg22@users.noreply.github.com>
2023-03-15 13:18:58 +0530

    Revert "Back and forth for course edit page"

--- project/ws/author/src/lib/routing/modules/editor/shared/components/edit-meta/edit-meta.component.ts
+++ project/ws/author/src/lib/routing/modules/editor/shared/components/edit-meta/edit-meta.component.ts
@@ -47,7 +47,7 @@ import {
   switchMap,
   map,
 } from 'rxjs/operators'
-import { Router } from '@angular/router'
+// import { Router } from '@angular/router'
 import { NSApiRequest } from '../../../../../../interface/apiRequest'
 
 // import { ApiService } from '@ws/author/src/lib/modules/shared/services/api.service'
@@ -68,7 +68,7 @@ export class EditMetaComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() stage = 1
   @Input() type = ''
   clickedBtnNext: boolean = false
-  saveTriggerSub?: Subscription
+
   location = CONTENT_BASE_STATIC
   selectable = true
   removable = true
@@ -154,7 +154,6 @@ export class EditMetaComponent implements OnInit, OnDestroy, AfterViewInit {
   isFormValid!: boolean
   competencies: any
   constructor(
-    // private storeService: CollectionStoreService,
     private formBuilder: FormBuilder,
     private uploadService: UploadService,
     private snackBar: MatSnackBar,
@@ -169,7 +168,7 @@ export class EditMetaComponent implements OnInit, OnDestroy, AfterViewInit {
     private accessService: AccessControlService,
     // private apiService: ApiService,
     private http: HttpClient,
-    private router: Router,
+    // private router: Router,
   ) {
 
 
@@ -355,12 +354,7 @@ export class EditMetaComponent implements OnInit, OnDestroy, AfterViewInit {
       if (this.contentMeta && this.canUpdate) {
         this.storeData()
       }
-      const url = this.router.url
-      const id = url.split('/')
-      this.editorService.readcontentV3(id[3]).subscribe((res: any) => {
-        this.contentMeta = res
-      })
-      this.content = this.contentService.getUpdatedMeta(id[3])
+      this.content = this.contentService.getUpdatedMeta(data)
     })
 
     // this.filteredOptions$ = this.keywordsCtrl.valueChanges.pipe(
@@ -529,9 +523,6 @@ export class EditMetaComponent implements OnInit, OnDestroy, AfterViewInit {
   ngOnDestroy() {
     if (this.routerSubscription) {
       this.routerSubscription.unsubscribe()
-    }
-    if (this.saveTriggerSub) {
-      this.saveTriggerSub.unsubscribe()
     }
     this.loader.changeLoad.next(false)
     this.ref.detach()
