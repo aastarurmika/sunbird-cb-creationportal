13d2d3d695063aeecefdbb846ab43604c393912e
likhithgowda <likhith.thammegowda@tarento.com>
2023-08-14 12:16:14 +0530

    changes in svg content

--- project/ws/author/src/lib/modules/shared/components/certificate-upload-dialog/certificate-upload-dialog.component.ts
+++ project/ws/author/src/lib/modules/shared/components/certificate-upload-dialog/certificate-upload-dialog.component.ts
@@ -51,13 +51,97 @@ export class CertificateDialogComponent implements OnInit {
       const year = date.getFullYear()
 
       const newIssuedDate = `${day}-${month}-${year}`
-
+      // let qrCode = "https://ibb.co/wNbdr4m"
       // Replace the content of the specified tspan elements
-      const last = svgContent.replace(/<tspan[^>]+>\${recipientName}<\/tspan>/g, `<tspan x="600" y="440">${newRecipientName}</tspan>`)
-        .replace(/<tspan[^>]+>\${issuedDate}<\/tspan>/g, `<tspan x="620" y="800">${newIssuedDate}</tspan>`)
+      // const last = svgContent.replace(/<tspan[^>]+>\${recipientName}<\/tspan>/g, `<tspan x="600" y="440">${newRecipientName}</tspan>`)
+      //   .replace(/<tspan[^>]+>\${issuedDate}<\/tspan>/g, `<tspan x="620" y="800">${newIssuedDate}</tspan>`)
       // let newSvgContent = svgContent.replace(/<tspan[^>]*>(.*?)<\/tspan>/, `<tspan x="600" y="440">Likhith</tspan>`).replace(/<tspan[^>]*>(.*?)<\/tspan>/, `<tspan x="620" y="800">10-08-2023</tspan>`)
+
+      // @ts-ignore: Unreachable code error
+      let bucket = window["env"]["sitePath"]
+
+      const newQrCodeImage = bucket + '/cbp-assets/images/qrCode.png'
+
+      // Create a DOMParser
+      const parser = new DOMParser()
+      const svgDoc = parser.parseFromString(svgContent, "image/svg+xml")
+
+      // Update or add recipient name
+      let recipientText = svgDoc.querySelector('text[id="${recipientName}"] tspan')
+      if (recipientText) {
+        recipientText.textContent = newRecipientName
+      } else {
+        const newTextElement = svgDoc.createElementNS("http://www.w3.org/2000/svg", "text")
+        newTextElement.setAttribute("id", "recipientName")
+        newTextElement.setAttribute("fill", "black")
+        newTextElement.setAttribute("xml:space", "preserve")
+        newTextElement.setAttribute("style", "white-space: pre")
+        newTextElement.setAttribute("font-family", "Roboto")
+        newTextElement.setAttribute("font-size", "48")
+        newTextElement.setAttribute("letter-spacing", "0em")
+
+        const tspanElement = svgDoc.createElementNS("http://www.w3.org/2000/svg", "tspan")
+        tspanElement.setAttribute("x", "600")
+        tspanElement.setAttribute("y", "440")
+        tspanElement.textContent = newRecipientName
+
+        newTextElement.appendChild(tspanElement)
+        svgDoc.documentElement.appendChild(newTextElement)
+      }
+
+      // Update or add QR code image
+      let qrCodeImageElement = svgDoc.querySelector('image[id="QrCode"]')
+      if (qrCodeImageElement) {
+        qrCodeImageElement.setAttribute("xlink:href", newQrCodeImage)
+      } else {
+        const newImageElement = svgDoc.createElementNS("http://www.w3.org/2000/svg", "image")
+        newImageElement.setAttribute("id", "QrCode")
+        newImageElement.setAttribute("class", "qr-code")
+        newImageElement.setAttribute("x", "600")
+        newImageElement.setAttribute("y", "620")
+        newImageElement.setAttribute("width", "150")
+        newImageElement.setAttribute("height", "150")
+        newImageElement.setAttribute("xlink:href", newQrCodeImage)
+
+        svgDoc.documentElement.appendChild(newImageElement)
+      }
+
+      // Update or add issued date
+      let issuedDateText = svgDoc.querySelector('text[id="${issuedDate}"] tspan')
+      if (issuedDateText) {
+        issuedDateText.textContent = newIssuedDate
+      } else {
+        const newTextElement = svgDoc.createElementNS("http://www.w3.org/2000/svg", "text")
+        newTextElement.setAttribute("id", "issuedDate")
+        newTextElement.setAttribute("fill", "black")
+        newTextElement.setAttribute("xml:space", "preserve")
+        newTextElement.setAttribute("style", "white-space: pre")
+        newTextElement.setAttribute("font-family", "Roboto")
+        newTextElement.setAttribute("font-size", "20")
+        newTextElement.setAttribute("letter-spacing", "0em")
+
+        const tspanElement = svgDoc.createElementNS("http://www.w3.org/2000/svg", "tspan")
+        tspanElement.setAttribute("x", "620")
+        tspanElement.setAttribute("y", "800")
+        tspanElement.textContent = newIssuedDate
+
+        newTextElement.appendChild(tspanElement)
+        svgDoc.documentElement.appendChild(newTextElement)
+      }
+
+      // Serialize the modified SVG back to a string
+      const modifiedSvgString = new XMLSerializer().serializeToString(svgDoc)
+
+      console.log("modifiedSvgString", modifiedSvgString)
+
+      // const lasts = svgContent
+      //   .replace(/\$\{recipientName\}/g, newRecipientName)
+      //   .replace(/\$\{qrCodeImage\}/g, "https://ibb.co/wNbdr4m")
+      //   .replace(/\$\{issuedDate\}/g, newIssuedDate)
+
       // Encode SVG to base64
-      const base64EncodedSvg = btoa(last)
+      // console.log("last", lasts)
+      const base64EncodedSvg = btoa(modifiedSvgString)
       // Add data URI prefix
       const dataUri = `data:image/svg+xml;base64,${base64EncodedSvg}`
       this.svgContent = this.sanitizer.bypassSecurityTrustResourceUrl(dataUri) as SafeResourceUrl
