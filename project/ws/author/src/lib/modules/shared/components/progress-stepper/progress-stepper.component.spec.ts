93f64eba904e5b6f7a9c5c43f5c1cf1e3fa74366
likhithgowda <likhith.thammegowda@tarento.com>
2024-02-05 16:44:17 +0530

    Unit test case for progress stepper

--- project/ws/author/src/lib/modules/shared/components/progress-stepper/progress-stepper.component.spec.ts
+++ project/ws/author/src/lib/modules/shared/components/progress-stepper/progress-stepper.component.spec.ts
@@ -1,16 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { AuthInitService } from '@ws/author/src/lib/services/init.service'
 import { ProgressStepperComponent } from './progress-stepper.component'
 
 describe('ProgressStepperComponent', () => {
   let component: ProgressStepperComponent
   let fixture: ComponentFixture<ProgressStepperComponent>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ProgressStepperComponent],
+      providers: [AuthInitService]
     }).compileComponents()
-  }))
+  })
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProgressStepperComponent)
@@ -18,7 +19,44 @@ describe('ProgressStepperComponent', () => {
     fixture.detectChanges()
   })
 
-  it('should create', () => {
-    expect(component).toBeTruthy()
+
+  describe('navigate', () => {
+
+    it('should not navigate when the current step is "Introduction"', () => {
+      const steps = [
+        { label: '1. Introduction', key: 'Introduction', activeStep: false, completed: true },
+        { label: '2. Course Details', key: 'CourseDetails', activeStep: true, completed: false },
+        { label: '3. Course Builder', key: 'CourseBuilder', activeStep: false, completed: false },
+        { label: '4. Course Settings', key: 'CourseSettings', activeStep: false, completed: false }
+      ]
+      component.steps = steps
+      spyOn(component.sendSteps, 'emit')
+      component.navigate('Introduction')
+      expect(component.sendSteps.emit).not.toHaveBeenCalled()
+    })
+
+    it('should not navigate when the current step is "AssessmentDetails"', () => {
+      const steps = [
+        { label: '1. Self Assessment Details', key: 'AssessmentDetails', activeStep: false, completed: true },
+        { label: '2. Self Assessment Builder', key: 'AssessmentBuilder', activeStep: true, completed: false },
+        { label: '3. Self Assessment Settings', key: 'AssessmentSettings', activeStep: false, completed: false }
+      ]
+      component.steps = steps
+      spyOn(component.sendSteps, 'emit')
+      component.navigate('AssessmentDetails')
+      expect(component.sendSteps.emit).not.toHaveBeenCalled()
+    })
+
+    it('should not navigate when the active index is 0', () => {
+      const steps = [
+        { label: '1. Self Assessment Details', key: 'AssessmentDetails', activeStep: false, completed: true },
+        { label: '2. Self Assessment Builder', key: 'AssessmentBuilder', activeStep: true, completed: false },
+        { label: '3. Self Assessment Settings', key: 'AssessmentSettings', activeStep: false, completed: false }
+      ]
+      component.steps = steps
+      spyOn(component.sendSteps, 'emit')
+      component.navigate('AssessmentDetails')
+      expect(component.sendSteps.emit).not.toHaveBeenCalled()
+    })
   })
 })
--- project/ws/author/src/lib/modules/shared/components/progress-stepper/progress-stepper.component.ts
+++ project/ws/author/src/lib/modules/shared/components/progress-stepper/progress-stepper.component.ts
@@ -8,7 +8,12 @@ import { AuthInitService } from '@ws/author/src/lib/services/init.service'
   styleUrls: ['./progress-stepper.component.scss'],
 })
 export class ProgressStepperComponent implements OnInit {
-  @Input() steps: any = '';
+  @Input() steps: any = [
+    { label: '1. Introduction', key: 'Introduction', activeStep: true, completed: false },
+    { label: '2. Course Details', key: 'CourseDetails', activeStep: false, completed: false },
+    { label: '3. Course Builder', key: 'CourseBuilder', activeStep: false, completed: false },
+    { label: '4. Course Settings', key: 'CourseSettings', activeStep: false, completed: false }
+  ];;
   @Input() header: any = ''
   @Output() sendSteps = new EventEmitter<any>();
   constructor(private initService: AuthInitService,
@@ -18,20 +23,24 @@ export class ProgressStepperComponent implements OnInit {
   ngOnInit() {
 
   }
+
   navigate(step: any) {
     console.log("AssessmentDetails", step)
-    if (step !== 'Introduction' && step !== 'AssessmentDetails') {
-      const activeIndex = this.steps.findIndex((step: { activeStep: boolean }) => step.activeStep === true)
-      const targetIndex = this.steps.findIndex((item: any) => item.key === step)
+    if (Array.isArray(this.steps) && this.steps.length > 0) {
+      if (step !== 'Introduction' && step !== 'AssessmentDetails') {
+        const activeIndex = this.steps.findIndex((step: { activeStep: boolean }) => step.activeStep === true)
+        const targetIndex = this.steps.findIndex((item: any) => item.key === step)
 
-      if (activeIndex > 0 && targetIndex < activeIndex) {
-        const previousStep = this.steps[activeIndex - 1]
-        console.log("Previous Step:", previousStep)
-        this.sendSteps.emit(previousStep.key)
-        this.initService.isBackButtonClickedAction('backButtonClicked')
+        if (activeIndex > 0 && targetIndex < activeIndex) {
+          const previousStep = this.steps[activeIndex - 1]
+          console.log("Previous Step:", previousStep)
+          this.sendSteps.emit(previousStep.key) // Emit the key instead of the label
+          this.initService.isBackButtonClickedAction('backButtonClicked')
+        }
       }
     }
-
   }
+
+
 
 }
