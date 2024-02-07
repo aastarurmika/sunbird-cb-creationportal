a130de70ca49259378364e5e22ea9050c64091a3
Surya Prakash G <surya.prakash@tarento.com>
2024-02-07 17:39:22 +0530

    cbp issues

--- project/ws/app/src/lib/routes/app-toc/components/app-toc-home/time-difference.pipe.ts
+++ project/ws/app/src/lib/routes/app-toc/components/app-toc-home/time-difference.pipe.ts
@@ -15,27 +15,27 @@ export class TimeDifferencePipe implements PipeTransform {
     // console.log(moment(new Date()).toISOString(), 'ss', moment(value).utcOffset(330))
     // console.log(moment.duration(moment(new Date()).diff(moment(value))))
     // console.log(moment(value).utcOffset(330))
-    let d1 = moment(new Date()).utcOffset(330)
-    let d2 = moment(value).utcOffset(330)
-    console.log(d2.toString(), moment(new Date()).toISOString())
-    const duration = moment.duration(moment(d1).diff(moment(d2)))
-    //console.log(moment().format('YYYY-MM-DD HH:mm:ss')), moment(new Date(value))
-    console.log(duration)
-    const days = duration.days()
-    const hours = duration.hours()
-    const minutes = duration.minutes()
-    console.log(days, hours, minutes)
-    let formattedString = ''
+    // let d1 = moment(new Date()).utcOffset(330)
+    // let d2 = moment(value).utcOffset(330)
+    // console.log(d2.toString(), moment(new Date()).toISOString())
+    // const duration = moment.duration(moment(d1).diff(moment(d2)))
+    // //console.log(moment().format('YYYY-MM-DD HH:mm:ss')), moment(new Date(value))
+    // console.log(duration)
+    // const days = duration.days()
+    // const hours = duration.hours()
+    // const minutes = duration.minutes()
+    // console.log(days, hours, minutes)
+    // let formattedString = ''
 
-    if (days > 0) {
-      formattedString += `${days} day${days !== 1 ? 's ago' : 'ago'} `
-    }
-    if (hours > 0) {
-      formattedString += `${hours} hour${hours !== 1 ? 's ago' : 'ago'} `
-    }
-    if (minutes > 0) {
-      formattedString += `${minutes} minute${minutes !== 1 ? 's ago' : 'ago'} `
-    }
+    // if (days > 0) {
+    //   formattedString += `${days} day${days !== 1 ? 's ago' : 'ago'} `
+    // }
+    // if (hours > 0) {
+    //   formattedString += `${hours} hour${hours !== 1 ? 's ago' : 'ago'} `
+    // }
+    // if (minutes > 0) {
+    //   formattedString += `${minutes} minute${minutes !== 1 ? 's ago' : 'ago'} `
+    // }
 
     const utcMoment = moment.utc(value) // Convert to UTC
     const localMoment = utcMoment.tz('Asia/Kolkata') // Convert to desired timezone
