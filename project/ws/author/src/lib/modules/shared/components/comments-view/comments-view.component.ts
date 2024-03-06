fcb1eafce173f0730362d7b69ead1c4dd0611df1
likhithgowda <likhith.thammegowda@tarento.com>
2024-03-06 11:27:27 +0530

    Reviewer checklist with comments

--- project/ws/author/src/lib/modules/shared/components/comments-view/comments-view.component.ts
+++ project/ws/author/src/lib/modules/shared/components/comments-view/comments-view.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
-import { NSContent } from '@ws/author/src/lib/interface/content'
 
 @Component({
   selector: 'ws-auth-root-comments-view',
